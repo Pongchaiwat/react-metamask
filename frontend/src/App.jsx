b98a5347c14f490ee7b6048a97c2fbda3fb8b1fe
Pongchaiwat <gun.pongchaiwat@gmail.com>
2022-03-07 21:26:57 -0500

    new commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { ActivateDeactivate } from './components/ActivateDeactivate';
+import { Greeter } from './components/Greeter';
+import { SectionDivider } from './components/SectionDivider';
+import { SignMessage } from './components/SignMessage';
+import { WalletStatus } from './components/WalletStatus';
+
+const StyledAppDiv = styled.div`
+  display: grid;
+  grid-gap: 20px;
+`;
+
+export function App() {
+  return (
+    <StyledAppDiv>
+      <ActivateDeactivate />
+      <SectionDivider />
+      <WalletStatus />
+      <SectionDivider />
+    </StyledAppDiv>
+  );
+}
